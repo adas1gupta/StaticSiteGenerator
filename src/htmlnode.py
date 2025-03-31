84ab5d4e1ee7b7b9c74fc6415a8cefcfa4b33d60
Arjun Dasgupta <dasgupta1arjun@gmail.com>
2025-03-30 20:01:35 -0400

    ParentNode class added with additional tests

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,13 +10,33 @@ class HTMLNode:
     
     def props_to_html(self):
         total = ''
+        if not self.props:
+            return total 
         for item in self.props:
-            total += f"{item}=\"{self.props[item]}\" "
+            total += f" {item}=\"{self.props[item]}\""
         
-        return total.rstrip(' ')
+        return total
     
     def __repr__(self):
         return f"HTMLNode({self.tag}, {self.value}, children: {self.children}, {self.props})"
+
+class ParentNode(HTMLNode):
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
+    
+    def to_html(self):
+        if not self.tag:
+            raise(ValueError("no tag"))
+        elif not self.children:
+            return (ValueError("no children")) 
+        else:
+            total_content = ''
+            for item in self.children:
+                total_content += item.to_html()
+            return f"<{self.tag}{self.props_to_html()}>{total_content}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
 
 class LeafNode(HTMLNode):
     def __init__(self, tag, value, props=None):
@@ -28,4 +48,7 @@ class LeafNode(HTMLNode):
         elif not self.tag:
             return self.value 
         else:
-            return f"<{self.tag}>{self.value}</{self.tag}>"
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f"LeafNode({self.tag}, {self.value}, {self.props})"
